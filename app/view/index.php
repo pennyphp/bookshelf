4aedc42796d627a1936ee49621782ae5c61a7846
Gianluca Arbezzano <gianarb92@gmail.com>
2015-07-23 14:30:08 +0200

    anchor href target

--- app/view/index.php
+++ app/view/index.php
@@ -2,13 +2,13 @@
 
 <div class="container">
   <div class="header clearfix">
-    <h3 class="text-muted"><a href="https://github.com/gianarb/penny">Penny Classic Application</a></h3>
+    <h3 class="text-muted"><a target="_blank" href"="https://github.com/gianarb/penny-classic-app">Penny Classic Application</a></h3>
   </div>
 
   <div class="jumbotron">
     <h1>Panny Classic Application</h1>
-    <p class="lead"><a href="https://gituhb.com/gianarb/penny">Penny</a> is a concentration of experience to build flexible and modern PHP application</p>
-    <p><a class="btn btn-lg btn-success" href="https://gituhb.com/gianarb/penny-classic-app" role="button">Try me!</a></p>
+    <p class="lead"><a target="_blank" href"="https://gituhb.com/gianarb/penny">Penny</a> is a concentration of experience to build flexible and modern PHP application</p>
+    <p><a class="btn btn-lg btn-success" target="_blank" href"="https://gituhb.com/gianarb/penny-classic-app" role="button">Try me!</a></p>
   </div>
 
   <div class="row marketing">
@@ -17,7 +17,7 @@
       <p>One penny is valueless but a lot of pennies build an empire.</p>
 
       <h4 style="color:green">Penny Dependencies</h4>
-      <p>Penny Core project depends from <a href="https://github.com/nikic/FastRoute">FastRouter</a> and <a href="http://php-di.org/">PHP-DI</a></p>
+      <p>Penny Core project depends from <a target="_blank" href"="https://github.com/nikic/FastRoute">FastRouter</a> and <a target="_blank" href"="http://php-di.org/">PHP-DI</a></p>
 
       <h4 style="color:green">Why?</h4>
       <p>In my opinion interoperabily is the future and I'm working to create a good solution to build application with a excellent quality</p>
@@ -25,10 +25,10 @@
 
     <div >
       <h4 style="color:green">Skeleton Application</h4>
-      <p><a href="https://github.com/gianarb/penny-classic-app">This skeleton application</a> is an example of penny implementations. This is a basic example to build "classic" app with an HTML views and other things</p>
+      <p><a target="_blank" href"="https://github.com/gianarb/penny-classic-app">This skeleton application</a> is an example of penny implementations. This is a basic example to build "classic" app with an HTML views and other things</p>
 
       <h4 style="color:green">Dependencies</h4>
-      <p>THIS Penny Skeleton builds integration between Penny and <a href="http://github.com/thephpleague/plates">thephpleague/plates</a> powerfull native php template system</p>
+      <p>THIS Penny Skeleton builds integration between Penny and <a target="_blank" href"="http://github.com/thephpleague/plates">thephpleague/plates</a> powerfull native php template system</p>
 
       <h4 style="color:green">Easy and Free</h4>
       <p>This project is open source and it's born because in this world exist a lot of beautiful OSS projects and a good developer must be able to choose her best tools!</p>
