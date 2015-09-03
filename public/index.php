33da501eaff6c44efd52b3150f3d0d5be4416c1b
Lorenzo Fontana <lorenzo.fontana@facile.it>
2015-09-03 15:20:48 +0200

    No more return

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
 chdir(dirname(__DIR__));
-require "vendor/autoload.php";
+require 'vendor/autoload.php';
 
 $app = new \GianArb\Penny\App();
 
-$template = $app->getContainer()->get("template");
+$template = $app->getContainer()->get('template');
 
-$app->getContainer()->get("http.flow")->attach("ERROR_DISPATCH", function ($event) use ($template) {
+$app->getContainer()->get('http.flow')->attach('ERROR_DISPATCH', function (\GianArb\Penny\Event\HttpFlowEvent $event) use ($template) {
     $e = $event->getException();
-    $response = $event->getResponse()->getBody()->write($template->render("error/40x", [
-        "title" => $e->getMessage(),
-        "exception" => $e,
+    $event->getResponse()->getBody()->write($template->render("error/40x", [
+        'title' => $e->getMessage(),
+        'exception' => $e,
     ]));
 });
 
