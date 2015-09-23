5007599f6f230034be9bc3bb6e554ae80213c8cf
Gianluca Arbezzano <gianarb92@gmail.com>
2015-09-23 21:44:04 +0200

    use php-di decorator

--- config/di.php
+++ config/di.php
@@ -1,5 +1,19 @@
 <?php
 return [
+    'event_manager' => \DI\decorate(function($eventManager, $container) {
+        $template = $contaner->get('template');
+        $eventManager->attach('ERROR_DISPATCH', function (\GianArb\Penny\Event\HttpFlowEvent $event) use ($template) {
+            $e = $event->getException();
+
+            $response = $event->getResponse()->withStatus($e->getCode());
+            $event->setResponse($response);
+
+            $response->getBody()->write($template->render("error/40x", [
+                'title' => $e->getMessage(),
+                'exception' => $e,
+            ]));
+        });
+    }),
     'template' => \DI\object(\League\Plates\Engine::class)->constructor('./app/view/'),
     'router' => function () {
         return \FastRoute\simpleDispatcher(function (\FastRoute\RouteCollector $r) {
--- public/index.php
+++ public/index.php
@@ -4,19 +4,5 @@ require 'vendor/autoload.php';
 
 $app = new \GianArb\Penny\App();
 
-$template = $app->getContainer()->get('template');
-
-$app->getContainer()->get('event_manager')->attach('ERROR_DISPATCH', function (\GianArb\Penny\Event\HttpFlowEvent $event) use ($template) {
-    $e = $event->getException();
-
-    $response = $event->getResponse()->withStatus($e->getCode());
-    $event->setResponse($response);
-
-    $response->getBody()->write($template->render("error/40x", [
-        'title' => $e->getMessage(),
-        'exception' => $e,
-    ]));
-});
-
 $emitter = new \Zend\Diactoros\Response\SapiEmitter();
 $emitter->emit($app->run());
