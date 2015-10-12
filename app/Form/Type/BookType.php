36669ed760659f54f8488c291e4225fd874d1fa3
Lorenzo Fontana <fontanalorenzo@me.com>
2015-10-12 08:21:51 +0200

    Import optimization

--- app/Form/Type/BookType.php
+++ app/Form/Type/BookType.php
@@ -2,7 +2,6 @@
 
 namespace ClassicApp\Form\Type;
 
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 use Penny\App;
-use Zend\Diactoros\Response\SapiEmitter;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
